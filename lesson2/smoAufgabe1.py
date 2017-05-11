48b4441ca96f745ea8edef9e4cfa612a94ada63c
Moritz <stefan.moritz@Thomas-MacBook-Pro.local>
2017-05-11 18:56:57 +0200

    Solved Excercise 1

--- lesson2/smoAufgabe1.py
+++ lesson2/smoAufgabe1.py
@@ -1,9 +1,38 @@
+import sys
+import datetime
+
+
 print ""
-print "Hello world."
-print "============"
+print "Talk to me"
+print "=========="
 print
+
+# ask user for name
 q = raw_input("Please enter your first name: ")
-print "Thank you, " + q
+print "Thank you, " + q + "."
 print
-p = raw_input("How old are you? ")
-print p + "!!! Just like me."
+
+# ask user for his age and birthday
+p = input("How old are you? ")
+
+if int(p) == 39:
+    print str(p) + "!!! Just like me."
+print
+c = raw_input("Did you celebrate your birthday this year already? (y/n): ")
+
+#calculate his year of birth
+if c == "y":
+    print "Congratulations!"
+    birth = datetime.datetime.now().year - int(p)
+else:
+    print "Don't forget to invite me."
+    birth = datetime.datetime.now().year - int(p) - 1
+    
+print    
+#give out the year of 100th birthday
+print q + ", your 100th birthday is in " + str(birth + 100) + "."
+print
+print "See you soon."
+print
+
+
