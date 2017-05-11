5affc0a34d6c1a3affbf8d2752b967489f768f5a
Moritz <stefan.moritz@Thomas-MacBook-Pro.local>
2017-05-11 18:04:33 +0200

    added Aufgabe 1

---
+++ lesson2/smoAufgabe1.py
@@ -0,0 +1,9 @@
+print ""
+print "Hello world."
+print "============"
+print
+q = raw_input("Please enter your first name: ")
+print "Thank you, " + q
+print
+p = raw_input("How old are you? ")
+print p + "!!! Just like me."
