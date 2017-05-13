999beee1af347985bd41fea8f8aef2d9b544d2f2
Creice <cdriesen@gmx.de>
2017-05-13 23:38:54 +0200

    Creice fixed his bug

--- lesson2/age_homework_chris.py
+++ lesson2/age_homework_chris.py
@@ -2,32 +2,32 @@
 ## Autor: Christian Driesen
 ##
 
-while True:
-    try:
-        age = raw_input("Bitte gib Dein Alter ein: ")
-        age = int(age)
-        if age <0:
-        	print ("Die Zahl muss groesser 0 sein! Versuche es noch ein Mal!")
-        	continue
-        else:
-        	break
-    except ValueError:
-        print("Bitte gib eine natuerliche Zahl groesser Null ein.")
+while True: ## ist eine Endlosschleife
+	try:
+		age = input ("Bitte gib Dein Alter ein: ")
+		age = int(age)
+		if age <0:
+			print ("Die Zahl muss groesser 0 sein! Versuche es noch ein Mal!")
+			continue
+		else:
+			break
+	except ValueError:
+		print("Bitte gib eine natuerliche Zahl groesser Null ein.")
 
 while True:
 	try:
-		bday_happen = raw_input ("Hattest Du schon Geburtstag (j/n)   ")
+		bday_happen = input ("Hattest Du schon Geburtstag (j/n)   ")
 		if bday_happen in ['j','n']:
 			break
 		else:
 			print ("Bitte gib j oder n ein ")
-		continue
+			continue
+	except ValueError:
+		print ("Bitte gib j oder n ein ")
 
-if bday_happen =='j':	
-	hundreds_bday = 100-age+2016
-else:
+if bday_happen =='j':
 	hundreds_bday = 100-age+2017
-
-print (hundreds_bday)
-
+else:
+	hundreds_bday = 100-age+2016
+print ("Dein Hundertster Geburtstag ist im Jahr  ",hundreds_bday)
 
