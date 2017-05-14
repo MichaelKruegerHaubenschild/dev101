7901a86d2235fa74344d6ce00358270278f9a180
Creice <cdriesen@gmx.de>
2017-05-14 23:00:14 +0200

    Chris Homework complete with corner cases

--- lesson2/age_homework_chris.py
+++ lesson2/age_homework_chris.py
@@ -14,16 +14,28 @@ while True: ## ist eine Endlosschleife
 	except ValueError:
 		print("Bitte gib eine natuerliche Zahl groesser Null ein.")
 
+if age %2: ## % Modulo Operator, wenn wahr = ungerade
+	print ("Dein Alter ist ungerade!")
+else:
+	print ("Dein Alter ist eine gerade Zahl!")
+
 while True:
 	try:
 		bday_happen = input ("Hattest Du schon Geburtstag (j/n)   ")
 		if bday_happen in ['j','n']:
+			if age == 0 and bday_happen =='n':
+				print ("Du bist 0 Jahre alt, aber existent, ergo hattest Du schon Geburtstag!") # Corner Case
+				bday_happen = 'j'
+			else:
+				for i in range (age): # range (stop) oder range (start,stop) oder range (start,stop,step)
+					print ("Happy Birthday!   ")
 			break
 		else:
 			print ("Bitte gib j oder n ein ")
 			continue
 	except ValueError:
 		print ("Bitte gib j oder n ein ")
+
 
 if bday_happen =='j':
 	hundreds_bday = 100-age+2017
