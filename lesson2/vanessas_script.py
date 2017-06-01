ca7a56a6fd01521d906fb4e80a29b5c2b77d3ca1
vanessa <vanessa.s.beck@gmail.com>
2017-06-01 17:25:59 +0200

    meine Hausaufgabe

---
+++ lesson2/vanessas_script.py
@@ -0,0 +1,15 @@
+print "hello world"
+name1 = raw_input("Please enter your name: ")
+print "Your name is " + name1
+
+name=raw_input("what's your name? :")
+what's your name? :Vanessa
+age=raw_input("how old are you?")
+how old are you? 26
+print name+age
+Vanessa  26
+year=2017-int(age)+100
+print"hello" + raw_input("what's your name: ") + raw_input("how old are you?") + "you will turn 100 in"+ str(year)
+what's your name: Vanessa
+how old are you? 26
+helloVanessa 26you will turn 100 in2091
