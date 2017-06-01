a36f81e99d00454924d46573f314df7c7d3b7a67
Caspar <caspar.wrede@gmail.com>
2017-06-01 22:20:54 +0200

    Added lesson 3

---
+++ lesson3/caspii_1.py
@@ -0,0 +1,7 @@
+number = input("Please enter a nunber: ")
+print "You entered " + str(number)
+
+if ((number % 2) == 0):
+    print "You entered an even number"
+else:
+    print "You entered an odd number"
