0151c35e15477d46494312d75fe758db92f4d37d
Caspar <caspar.wrede@gmail.com>
2017-06-06 17:09:04 +0200

    Added lesson 4

---
+++ lesson4/caspi_1.py
@@ -0,0 +1,17 @@
+# print "Hello world"
+#
+# def add_2_numbers(num1, num2):
+#     new_number = num1 + num2
+#     return new_number
+#
+# print add_2_numbers(90,34)
+# print add_2_numbers(1,34)
+# print add_2_numbers(45, -100)
+
+def greet(name):
+    print "Hello " + name + ", I hope you are well"
+    return 1
+
+greet('caspar')
+keks = greet('crhis')
+print keks
