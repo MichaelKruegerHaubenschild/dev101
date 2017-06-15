8d61d33065f94c9ae0f2805e4da857ac6382696b
Christian Lindner <lindner_c@gmx.de>
2017-06-15 18:05:57 +0200

    Nachbesserung

--- lesson5/Karlfunkel_2.py
+++ lesson5/Karlfunkel_2.py
@@ -1,7 +1,7 @@
 def list_info(my_list):
     print "Die Laenge der Liste ist " + str(len(my_list))
     print "Das erste Element der Liste ist " + str(a[0])
-    print "Das letzte Element der Liste ist " + str(a[4])
+    print "Das letzte Element der Liste ist " + str(a[len(my_list)-1])
 
-a = [0,23,34,2200,23]
+a = [0,23,34,2200,23,67776]
 list_info(a)
