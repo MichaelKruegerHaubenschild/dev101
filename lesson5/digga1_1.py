c006b8e2ffabbc72282bf77cb2b1ac79f862a4b0
peter <peter93587@gmail.com>
2017-06-20 17:35:56 +0200

    update aufgabe

--- lesson5/digga1_1.py
+++ lesson5/digga1_1.py
@@ -5,5 +5,16 @@ def greet (name, BeNice):
         reply = name + ", you are not so nice!"
     return reply
 
-print greet("Peter", True)
-print greet("Peter", 1)
+def mult (text, how_often):
+    mult = how_often * text
+    return  mult
+
+def listinfo (mylist):
+    length = len(mylist)
+    print "This is a list. The length is " + str(length)
+    print " the first element is: " + str(mylist[0])
+    print " the last one is: " + str(mylist[length -1])
+    return "Hallo"
+
+mylist = [1,2,3,4,5]
+listinfo ([1,2,3,4,5])
