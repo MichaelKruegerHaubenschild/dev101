74f6e52229b5e4a662a439e4e4115e0376b6ad03
peter <peter93587@gmail.com>
2017-06-21 14:03:48 +0200

    Update zweite Lösung

--- lesson5/digga1_3.py
+++ lesson5/digga1_3.py
@@ -2,13 +2,24 @@ def less_than_10(Numlist):
     NewList = []
 
     for n in Numlist:
-        
+
         if n < 10:
             NewList.append(n)
 
     return NewList
 
 
-list = [1,3,5,11,22,1,1,100,3]
 
-print less_than_10(list)
+def less_than_10_V2(Numlist):
+
+    for n in reversed(Numlist):
+
+        if n > 10:
+            Numlist.remove(n)
+
+    return Numlist
+
+
+list = [1,3,5,11,22,1,1,100,111,222,13,4,5,11111,1]
+
+print less_than_10_V2(list)
