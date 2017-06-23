7b231e890503f944142f618195a4aa2691739f2b
Caspar <caspar.wrede@gmail.com>
2017-06-23 22:01:06 +0200

    Implemented Monte Carlo Simulation for 3-Tore Rätsel

---
+++ lesson6/monte_carlo.py
@@ -0,0 +1,41 @@
+from random import randint
+
+# SCHRITT 1
+# Liste von simulierten Spielen erzeugen
+spiele = []
+ANZ_SPIELE = 100000
+for a in range(1, ANZ_SPIELE):
+    spiel = [False, False, False]
+    spiel[randint(0,2)] = True
+    spiele.append(spiel)
+
+# SCHRITT 2
+# 2 Methoden fuer die 2 Strategien
+def nicht_wechseln_strategie(spiel):
+    meine_wahl = randint(0,2)
+    if spiel[meine_wahl] == True:
+        return 1
+    else:
+        return 0
+
+def wechseln_strategie(spiel):
+    meine_wahl = randint(0,2)
+    if spiel[meine_wahl] == False:
+        return 1
+    else:
+        return 0
+
+# SCHRITT 3
+# Durch Spiele Liste durch iterieren und Zaehlen,
+# welche strategie wie oft gewinnt
+wechseln = 0
+nicht_wechseln = 0
+for spiel in spiele:
+    wechseln = wechseln + wechseln_strategie(spiel)
+    nicht_wechseln = nicht_wechseln + nicht_wechseln_strategie(spiel)
+
+# SCHRITT 4
+# Ergennisse ausgeben
+print "Es wurden %s Spiele gespielt" % ANZ_SPIELE
+print "  Wechseln Strategie Siege: " + str(wechseln)
+print "  Nicht wechseln Strategie Siege: " + str(nicht_wechseln)
